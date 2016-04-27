66dda78aa25725fa4429093ecc9adb84abed38d3
Jean-Noël Rouvignac <jean-noel.rouvignac@forgerock.com>
2016-04-27 16:39:05 +0200

    commands_ldap.sh: change the default build location

--- commands_ldap.sh
+++ commands_ldap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
 # LDAP search
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "(uid=bjensen)"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "(uid=bjensen)"
 curl "http://localhost:8080/users/bjensen?_prettyPrint=true"
 
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "&"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "&"
 curl "http://localhost:8080/users?_queryFilter=true&_prettyPrint=true"
 
 
 # LDAP modify
 # create user
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -a -f ~/ldif/newuser.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -a -f ~/ldif/newuser.ldif
 # create user inline
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -a <<END_OF_COMMAND_INPUT
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -a <<END_OF_COMMAND_INPUT
 dn: cn=A1,dc=example,dc=com
 objectclass:top
 objectclass:organizationalperson
@@ -26,17 +26,17 @@ mail:bla@example.com
 telephonenumber:+33165990803
 END_OF_COMMAND_INPUT
 # add description attribute
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/newdesc.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/newdesc.ldif
 # modify description 1 attribute
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/moddesc1.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/moddesc1.ldif
 # modify description 2 attribute
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/moddesc2.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/moddesc2.ldif
 # make description attribute multivalued
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/multivalueddesc.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/multivalueddesc.ldif
 # delete user
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/deluser.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin    -f ~/ldif/deluser.ldif
 # display the newly added user
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "(uid=newuser)"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin -T -b "dc=example,dc=com" "(uid=newuser)"
 
 
 # REST using authentication
@@ -47,62 +47,62 @@ curl "http://bjensen:hifalutin@localhost:8080/users/newuser?_prettyPrint=true"
 
 # dsconfig HTTP Connection Handler
 # a bidouiller tools.properties dans le home???
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X     --displayCommand --advanced
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X     --displayCommand --advanced
 
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set enabled:true
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set authentication-required:false
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based HTTP Access Logger" --set enabled:true
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set suppress-internal-operations:false
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set log-format:"cs-host c-ip cs-username datetime cs-method cs-uri-query cs-version sc-status sc-bytes cs(User-Agent) x-connection-id" &
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set enabled:true
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set authentication-required:false
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based HTTP Access Logger" --set enabled:true
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set suppress-internal-operations:false
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set log-format:"cs-host c-ip cs-username datetime cs-method cs-uri-query cs-version sc-status sc-bytes cs(User-Agent) x-connection-id" &
 
 # enable debug logs
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Debug Logger"  --set default-debug-level:all --set enabled:true
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Debug Logger"  --set default-debug-level:all --set enabled:true
 # create debug target
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  create-debug-target         --publisher-name "File-Based Debug Logger"  --set debug-level:all --type generic --target-name org.opends.server.api
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  create-debug-target         --publisher-name "File-Based Debug Logger"  --set debug-level:all --type generic --target-name org.opends.server.api
 
 # stats / Performance
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin  -T -b "cn=monitor" "(objectClass=ds-connectionhandler-statistics-monitor-entry)"
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin  -T -b "cn=HTTP Connection Handler 0.0.0.0 port 8080 Statistics,cn=monitor" "(objectClass=*)"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin  -T -b "cn=monitor" "(objectClass=ds-connectionhandler-statistics-monitor-entry)"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -D "cn=Directory Manager" -w admin  -T -b "cn=HTTP Connection Handler 0.0.0.0 port 8080 Statistics,cn=monitor" "(objectClass=*)"
 bin/modrate -p 1500 -D "cn=directory manager" -w admin -F -c 4 -t 4 -b "uid=user.%d,ou=people,dc=example,dc=com"     -g "rand(0,2000)" -g "randstr(16)" 'description:%2$s'
 
 
 # status
-build/package/OpenDJ-2.7.0_auto/bin/status        -w admin -X    -D "cn=Directory Manager"
+target/package/opendj_auto/bin/status        -w admin -X    -D "cn=Directory Manager"
 # replication
-build/package/OpenDJ-2.7.0_auto/bin/dsreplication -w admin -X -n -b "dc=example,dc=com" status
+target/package/opendj_auto/bin/dsreplication -w admin -X -n -b "dc=example,dc=com" status
 
 
 # Processing time test
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set enabled:true
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set authentication-required:false
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based HTTP Access Logger" --set enabled:true
-build/package/OpenDJ-2.7.0_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set suppress-internal-operations:false
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set enabled:true
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-connection-handler-prop --handler-name "HTTP Connection Handler"    --set authentication-required:false
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based HTTP Access Logger" --set enabled:true
+target/package/opendj_auto/bin/dsconfig --hostname localhost -p 4444 -D "cn=Directory Manager" -w admin -X -n  set-log-publisher-prop      --publisher-name "File-Based Access Logger" --set suppress-internal-operations:false
 curl "http://bjensen:hifalutin@localhost:8080/users?_queryFilter=true&_prettyPrint=true"
-for i in {5..12}; do grep conn=${i} build/package/OpenDJ-2.7.0_auto/logs/access | perl -ne 'print "$1\n" if (m/etime=(\d+)/);' | paste -sd+ | bc; done
+for i in {5..12}; do grep conn=${i} target/package/opendj_auto/logs/access | perl -ne 'print "$1\n" if (m/etime=(\d+)/);' | paste -sd+ | bc; done
 
 
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -f ~/ldif/OPEND-948_aci.ldif
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -T -b "dc=example,dc=com" "&"
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -T -b "cn=this does not exist,ou=people,dc=example,dc=com" "objectclass=*"
-build/package/OpenDJ-2.7.0_auto/bin/ldapdelete -p 1389 "uid=user.9,ou=people,dc=example,dc=com"
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -f ~/ldif/OPEND-948_modify_user_entry.ldif
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1389 -T -b "ou=people,dc=example,dc=com" "objectclass=*" debugsearchindex
-build/package/OpenDJ-2.7.0_auto/bin/ldapmodify -p 1389 -a -f ~/ldif/OPEND-948_existing_user_entry.ldif
+target/package/opendj_auto/bin/ldapmodify -p 1389 -D "cn=Directory Manager" -w admin -f ~/ldif/OPEND-948_aci.ldif
+target/package/opendj_auto/bin/ldapsearch -p 1389 -T -b "dc=example,dc=com" "&"
+target/package/opendj_auto/bin/ldapsearch -p 1389 -T -b "cn=this does not exist,ou=people,dc=example,dc=com" "objectclass=*"
+target/package/opendj_auto/bin/ldapdelete -p 1389 "uid=user.9,ou=people,dc=example,dc=com"
+target/package/opendj_auto/bin/ldapmodify -p 1389 -f ~/ldif/OPEND-948_modify_user_entry.ldif
+target/package/opendj_auto/bin/ldapsearch -p 1389 -T -b "ou=people,dc=example,dc=com" "objectclass=*" debugsearchindex
+target/package/opendj_auto/bin/ldapmodify -p 1389 -a -f ~/ldif/OPEND-948_existing_user_entry.ldif
 
 # replication
 
 bin/modrate -p 1500 -D "cn=directory manager" -w admin --noRebind --numConnections 4 --numThreads 4 --maxIterations 16  \
             -b "uid=user.%d,ou=people,dc=example,dc=com" --argument "inc(0,500000)" --argument "randstr(16)" 'description:%2$s'
 # search on changelog
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "&" "*" "+" | less
+target/package/opendj_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "&" "*" "+" | less
 # persistent search on changelog
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -C ps:all -T -b "cn=changelog" "&" "(objectclass=*)" | less
+target/package/opendj_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -C ps:all -T -b "cn=changelog" "&" "(objectclass=*)" | less
 # search on changelog with changenumber
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "changenumber>=1" "*" "+"
+target/package/opendj_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "changenumber>=1" "*" "+"
 # search on changelog with changelogcookie
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "changelogcookie=...cookie..." "*" "+"
+target/package/opendj_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "cn=changelog" "changelogcookie=...cookie..." "*" "+"
 # search on lastchangenumber virtual attribute
-build/package/OpenDJ-2.7.0_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "" -s base "&" lastchangenumber
+target/package/opendj_auto/bin/ldapsearch -p 1501 -D "cn=Directory Manager" -w admin -T -b "" -s base "&" lastchangenumber
 
 
 
