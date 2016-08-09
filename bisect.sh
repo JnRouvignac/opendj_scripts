26e5e79445204b4888b00f3722755869462456b4
Jean-Noël Rouvignac <jean-noel.rouvignac@forgerock.com>
2016-08-09 12:05:30 +0200

    Update bisect.sh
    - bisect.sh is now for `git bisect` rather than `svn-bisect`
    - OpenDJ is now built with maven
    - update pyforge path to my new machine
    - also allow to run setup_replication.sh

--- bisect.sh
+++ bisect.sh
@@ -1,25 +1,73 @@
-#!/bin/bash
+#!/bin/bash -x
+# -e will fail the script if any command fails. It might be too constraining for some scripts
+# -x echoes each command before running it. It can be disabled temporarily with 'set +x'.
 
-# svn-bisect --min 9067 --max 10635 start
-# svn-bisect run ./run.sh
+PS4='\n+ Line ${LINENO}: ' # -x outputs is prefixed with newline and LINENO
+
+
+
+# git bisect start BAD_COMMIT GOOD_COMMIT_1 [...  GOOD_COMMIT_N]
+
+echo <<START_BISECT_SESSION
+
+ git bisect start 98fa91eb1a7 e535e3f5281
+ git bisect run ~/scripts/bisect.sh 2>&1 | tee bisect_out.txt
+
+ # visualize the remaining suspects
+ # git bisect view
+ # review what has been done in this bisect session
+ # git bisect log
+
+START_BISECT_SESSION
+
+
+
+run_pyforge()
+{
+    cd ~/git/pyforge
+    if [ $? -ne 0 ]
+    then
+        exit 125 # stop bisecting
+    fi
+
+    ./run-pybot.py --suite testcases.replication.externalchangelog .
+    FINAL_RESULT=$?
+
+    cd -
+
+    exit $FINAL_RESULT
+}
+
+
+
+run_setup_replication()
+{
+    cd ~/git/opendj/opendj-server-legacy
+    if [ $? -ne 0 ]
+    then
+        exit 125 # stop bisecting
+    fi
+
+    ~/scripts/setup_replication.sh
+    FINAL_RESULT=$?
+
+    cd -
+
+    exit $FINAL_RESULT
+}
+
+
+#####################
+# EXECUTE bisect.sh #
+#####################
 
 ~/scripts/killall_servers.sh;
 
-ant clean package;
+mvn clean install -Dmaven.test.skip;
 if [ $? -ne 0 ]
 then
     exit 125 # skip revision
 fi
 
-cd /ssddata/svn/QA-OpenDJ/robot-framework/trunk
-if [ $? -ne 0 ]
-then
-    exit 125 # stop bisecting
-fi
+run_setup_replication
 
-./run-pybot.sh --suite testcases.replication.externalchangelog .
-FINAL_RESULT=$?
-
-cd -
-
-exit $FINAL_RESULT
