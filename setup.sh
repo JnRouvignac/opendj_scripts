c7ac8ea13a340d0cd0983543b75594869624cf9c
Jean-Noël Rouvignac <jean-noel.rouvignac@forgerock.com>
2018-07-25 11:30:24 +0200

    Use ~/git/pyforge/archives to read the archives

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@
 PS4='\n+ Line ${LINENO}: ' # -x outputs is prefixed with newline and LINENO
 
 BUILD_DIR=`pwd`
-ZIP_2_5_0=~/Downloads/OpenDJ-2.5.0-Xpress1.zip
-ZIP_2_6_0=~/Downloads/OpenDJ-2.6.0.zip
-ZIP_3_0_0=~/Downloads/OpenDJ-3.0.0.zip
-ZIP_3_5_0=~/Downloads/opendj-3.5.0.zip
-ZIP_4_0_0=~/Downloads/opendj-4.0.0.zip
-ZIP_5_0_0=~/Downloads/opendj-4.0.0.zip
-ZIP_5_5_0=~/Downloads/DS-5.5.0.zip
-ZIP_6_0_0=~/Downloads/DS-6.0.0.zip
+ZIP_2_5_0=~/git/pyforge/archives/OpenDJ-2.5.0-Xpress1.zip
+ZIP_2_6_0=~/git/pyforge/archives/OpenDJ-2.6.0.zip
+ZIP_3_0_0=~/git/pyforge/archives/OpenDJ-3.0.0.zip
+ZIP_3_5_0=~/git/pyforge/archives/opendj-3.5.0.zip
+ZIP_4_0_0=~/git/pyforge/archives/opendj-4.0.0.zip
+ZIP_5_0_0=~/git/pyforge/archives/opendj-4.0.0.zip
+ZIP_5_5_0=~/git/pyforge/archives/DS-5.5.0.zip
+ZIP_6_0_0=~/git/pyforge/archives/DS-6.0.0.zip
 BUILDING_35X=
 if [ "${BUILDING_35X}" = false ]
 then
--- upgrade.sh
+++ upgrade.sh
@@ -5,14 +5,14 @@
 PS4='\n+ Line ${LINENO}: ' # -x outputs is prefixed with newline and LINENO
 
 BUILD_DIR=`pwd`
-ZIP_2_5_0=~/Downloads/OpenDJ-2.5.0-Xpress1.zip
-ZIP_2_6_0=~/Downloads/OpenDJ-2.6.0.zip
-ZIP_3_0_0=~/Downloads/OpenDJ-3.0.0.zip
-ZIP_3_5_0=~/Downloads/opendj-3.5.0.zip
-ZIP_4_0_0=~/Downloads/opendj-4.0.0.zip
-ZIP_5_0_0=~/Downloads/opendj-4.0.0.zip
-ZIP_5_5_0=~/Downloads/DS-5.5.0.zip
-ZIP_6_0_0=~/Downloads/DS-6.0.0.zip
+ZIP_2_5_0=~/git/pyforge/archives/OpenDJ-2.5.0-Xpress1.zip
+ZIP_2_6_0=~/git/pyforge/archives/OpenDJ-2.6.0.zip
+ZIP_3_0_0=~/git/pyforge/archives/OpenDJ-3.0.0.zip
+ZIP_3_5_0=~/git/pyforge/archives/opendj-3.5.0.zip
+ZIP_4_0_0=~/git/pyforge/archives/opendj-4.0.0.zip
+ZIP_5_0_0=~/git/pyforge/archives/opendj-4.0.0.zip
+ZIP_5_5_0=~/git/pyforge/archives/DS-5.5.0.zip
+ZIP_6_0_0=~/git/pyforge/archives/DS-6.0.0.zip
 ZIP_MASTER=`ls ${BUILD_DIR}/target/*pen*-*.zip`
 
 ZIP=${ZIP_5_5_0}
@@ -20,7 +20,7 @@ ZIP2=${ZIP_MASTER}
 
 DATETIME=`date +%Y%m%d_%H%M%S`
 SETUP_DIR="${BUILD_DIR}/target/opendj_auto"
-HOSTNAME=localhost
+HOSTNAME=`hostname`
 ADMIN_PORT=4444
 DEBUG_PORT=8000
 BIND_DN="cn=Directory Manager"
@@ -65,7 +65,7 @@ then
 fi
 
 SETUP_ARGS="-d 1000"
-if [ "{$ZIP}" != "${ZIP_2_5_0}" ]
+if [ "${ZIP}" != "${ZIP_2_5_0}" ]
 then
     SETUP_ARGS="${SETUP_ARGS} --acceptLicense"
 fi
