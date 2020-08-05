6d07089d0b08206dda2f0eda210114ac67954453
Jean-Noël Rouvignac <jean-noel.rouvignac@forgerock.com>
2020-08-05 14:12:33 +0200

    Add useful git commands

--- commands_unix.sh
+++ commands_unix.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
 
+# gitk show only local branches
+gitk --argscmd='git for-each-ref --format="%(refname)" refs/heads'
+
+# visualize git orphan commits
+gitk --all `git reflog | cut -c1-7` &
+
 # restore WiFi after suspend
 sudo systemctl restart network-manager
+
