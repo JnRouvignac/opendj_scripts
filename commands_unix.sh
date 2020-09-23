299ccede8fe3620fb772fd584d41f2493d60ddf9
Jean-Noël Rouvignac <jean-noel.rouvignac@forgerock.com>
2020-09-23 11:16:51 +0200

    fix eclipse .classpath

--- commands_unix.sh
+++ commands_unix.sh
@@ -9,3 +9,7 @@ gitk --all `git reflog | cut -c1-7` &
 # restore WiFi after suspend
 sudo systemctl restart network-manager
 
+# fix eclipse .classpath
+find . -name .classpath | xargs sed 's/26.0.0-20191206110111-136b5fa/26.1.0-20200918174936-87989f3/g' -i
+find . -name .classpath | xargs sed 's@M2_REPO/org/assertj/assertj-core/3.6.2/assertj-core-3.6.2@M2_REPO/org/assertj/assertj-core/3.15.0/assertj-core-3.15.0@g' -i
+
